010d314567227bf1788a27f8601b2e2cae04b080
zhnhou <zhn.hou@gmail.com>
2016-08-06 16:41:08 -0500

    now play with the ratio with color bar

--- MCMC.analysis/examples/posterior2d_spt_bao_h0.py
+++ MCMC.analysis/examples/posterior2d_spt_bao_h0.py
@@ -1,58 +1,19 @@
 import matplotlib.pyplot as plt
-from make_figures import *
-
-
 
 if __name__ == '__main__':
     if __package__ is None:
         from os import path
         import sys
-        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) )+'/source' )
         
-        from mcmc_analysis import *
+        from make_figures import *
     else:
-        from ..mcmc_analysis import *
+        from source.make_figures import *
 
     s12tau_chain_path = '/Users/zhenhou/Downloads/lcdm_camb_s12tau/'
 
-    pdf_file = '../../figures/posterior_spt_bao_h0.pdf'
+    pdf_file = 'figures/posterior_spt_bao_h0.pdf'
     
-    x_param = 'H0'
-    y_param = 'Dv057rs'
-    z_param = 'Omegamh2'
-
-    feature = [x_param, 'Dv_0.57', 'rs_zdrag', z_param]
-
-    m = mcmc_analysis(chain_path=s12tau_chain_path, feature=feature)
-    m.chain['Dv057rs'] = 100.0 * m.chain['rs_zdrag'] / m.chain['Dv_0.57']
-
-    fp = FigProp_CMBBAOH0()
-
-    fig, ax = plt.subplots()
-
-    plt.rc('font', family='serif')
-
-    x_select, y_select, z_select = m.create_2d_scatter(x_param, y_param, z_param, num_select=10000)
-    scatter = plt.scatter(x_select, y_select, c=z_select, s=1, vmin=0.105, vmax=0.155, edgecolors='face', cmap=plt.get_cmap('YlGnBu_r'))
-
-    X, Y, Z = m.create_2d_gmm(x_param, y_param)
-
-    z, clevels = calc_clevel_gmm(Z)
-
-    plt.contour(X, Y, z, levels=clevels, colors='k')
-
-    ax.set_xlim([fp.xmin, fp.xmax])
-    ax.set_ylim([fp.ymin, fp.ymax])
-
-    ax.set_xticks(fp.xticks)
-    ax.set_yticks(fp.yticks)
-    
-    ax.set_xlabel(fp.xlabel, fontsize=16)
-    ax.set_ylabel(fp.ylabel, fontsize=16)
-
-    ax.set_xticklabels(fp.xticklabels, fontsize=16)
-    ax.set_yticklabels(fp.yticklabels, fontsize=16)
-
-    ax.set_aspect((fp.xmax-fp.xmin)/(fp.ymax-fp.ymin))
-
-    plt.savefig(pdf_file, format='pdf')
+    fig = Figure_CMBBAOH0('SPT', s12tau_chain_path, pdf_file=pdf_file, NoColorBar=False)
+    fig.process_chain()
+    fig.create_figure()
--- MCMC.analysis/source/make_figures.py
+++ MCMC.analysis/source/make_figures.py
@@ -1,5 +1,8 @@
 import numpy as np
 from scipy.special import *
+from mcmc_analysis import *
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def calc_clevel_gmm(logprob):
     z = np.exp(-logprob)
@@ -32,7 +35,7 @@ class FigProp_CMBBAOH0(object):
         self.yticks = [7, 8, 9]
 
         self.xlabel = r'$H_0\;[\mathrm{km\,s^{-1}\,Mpc^{-1}}]$'
-        self.ylabel = r'$10^2r_s(z_{\mathrm{drag}}) / D_v(0.57)$'
+        self.ylabel = r'$10^2r_s(z_{\mathrm{drag}}) / D_{\mathrm{v}}(0.57)$'
 
         xticklabels = []
         for x in self.xticks:
@@ -46,15 +49,70 @@ class FigProp_CMBBAOH0(object):
         self.yticklabels = yticklabels
 
 class Figure_CMBBAOH0(object):
-    def __init__(self, cmb_type, chain_path, z_param='Omegamh2', NoColorBar=False, NoYticks=False):
+    def __init__(self, cmb_type, chain_path, z_param='Omegamh2', NoColorBar=False, NoYticks=False, pdf_file='figure.pdf'):
 
         self.chain_path = chain_path
         self.cmb_type = cmb_type
         self.x_param = 'H0'
         self.y_param = 'rsDv057'
-        self.z_param = 'z_param'
+        self.z_param = z_param
+        self.pdf_file = pdf_file
 
         self.NoColorBar = NoColorBar
         self.NoYticks = NoYticks
 
+    def process_chain(self):
+        
+        feature = ['H0', 'Dv_0.57', 'rs_zdrag', self.z_param]
+        self.mcmc = mcmc_analysis(chain_path=self.chain_path, feature=feature)
+        self.mcmc.chain['rsDv057'] = 100.0 * self.mcmc.chain['rs_zdrag'] / self.mcmc.chain['Dv_0.57']
+
+
+    def create_figure(self):
+
+        fp = FigProp_CMBBAOH0()
+
+        fig, ax = plt.subplots()
+
+        ax.set_position([0.1,0.125,0.8,0.8])
+
+        x_select, y_select, z_select = self.mcmc.create_2d_scatter(self.x_param, self.y_param, self.z_param, num_select=10000)
+        scatter = plt.scatter(x_select, y_select, c=z_select, s=1, vmin=0.105, vmax=0.155, edgecolors='face', cmap=plt.get_cmap('YlGnBu_r'))
+
+        X, Y, Z = self.mcmc.create_2d_gmm(self.x_param, self.y_param)
+
+        z, clevels = calc_clevel_gmm(Z)
+
+        plt.contour(X, Y, z, levels=clevels, colors='k')
+
+#        ax.set_aspect((fp.xmax-fp.xmin)/(fp.ymax-fp.ymin))
+
+        if (not self.NoColorBar):
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.2)
+            
+#            cbar = fig.colorbar(scatter, ticks=(-100,-50,0,50,100), orientation='vertical', cax=cax,
+            cbar = fig.colorbar(scatter, orientation='vertical', cax=cax,
+                   drawedges=False)
+
+            cbar = fig.colorbar(scatter, orientation='vertical', cax=cax, drawedges=False)
+            
+            cbar.solids.set_edgecolor("face")
+#            cbar.ax.set_ylabel(' ', fontsize=16)
+#            cbar.ax.axes.set_yticklabels(["$-100$","$-50$","$0$","$50$","$100$"], fontsize=16)
+            cbar.outline.set_edgecolor('black')
+            cbar.outline.set_linewidth(1.2)
+
+        ax.set_xlim([fp.xmin, fp.xmax])
+        ax.set_ylim([fp.ymin, fp.ymax])
+
+        ax.set_xticks(fp.xticks)
+        ax.set_yticks(fp.yticks)
     
+        ax.set_xlabel(fp.xlabel, fontsize=16)
+        ax.set_ylabel(fp.ylabel, fontsize=16)
+
+        ax.set_xticklabels(fp.xticklabels, fontsize=16)
+        ax.set_yticklabels(fp.yticklabels, fontsize=16) 
+
+        plt.savefig(self.pdf_file, format='pdf')
