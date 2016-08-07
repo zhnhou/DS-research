ee37ea42159dfc646b4eabaa182c4a0f2e8c53d8
zhnhou <zhn.hou@gmail.com>
2016-08-07 17:29:39 -0500

    more progress

---
+++ spt.planck.cv/example/plot_spt_fields.py
@@ -0,0 +1,18 @@
+import healpy as hp
+
+if __name__ == '__main__':
+    if __package__ is None:
+        from os import path
+        import sys
+        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) )+'/source' )
+
+        from make_figures import *
+    else:
+        from source.make_figures import *
+
+    file_hfi143 = 'data/HFI_SkyMap_RING_143_2048_R2.00_full.fits'
+    map_hfi143 = hp.read_map(file_hfi143) * 1e6
+
+    spt = Healpix_SPTField(2048, background=map_hfi143)
+    spt.create_background()
+
--- spt.planck.cv/source/make_figures.py
+++ spt.planck.cv/source/make_figures.py
@@ -12,6 +12,13 @@ class Healpix_SPTField(object):
             self.background = np.zeros(hp.nside2npix(self.nside))
             self.background[:] = -1.6375e30
 
+    def vec2pix_ring(self, x, y, z):
+        return hp.vec2pix(self.nside,x,y,z)
 
     def create_background(self):
-        hp.orthview(map=self.background, rot=[0,270,0], coord=['G','C'])
+#        self.projorth = hp.projector.SphericalProj()
+#        self.orth_map = self.projorth.projmap(self.background, self.vec2pix_ring)
+#        plt.imsave('test.png', self.orth_map, cmap="RdBu", vmin=-400, vmax=400)
+
+        hp.orthview(map=self.background, rot=[0,270,0], coord=['G','C'], half_sky=True, notext=True, min=-400, max=400, xsize=1600)
+        hp.graticule()
